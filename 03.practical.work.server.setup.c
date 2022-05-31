5aa906d9f1b995cd7ccada057a46b65e26ad17b9
reijiken <quyduongtran0810@gmail.com>
2022-05-31 15:46:17 +0700

    Update 03.practical.work.server.setup.c
    fix lab3

--- 03.practical.work.server.setup.c
+++ 03.practical.work.server.setup.c
@@ -6,45 +6,43 @@
 
 int main(int argc, char const* argv[])
 {
-int sockfd , clen , clientfd;
-struct sockaddr_in saddr , caddr;
-char* hello = "Hello ";
-char buffer[1024] = { 0 };
-unsigned short port = 8784;
-if (( sockfd = socket(AF_INET , SOCK_STREAM , 0)) <0)
-{
-    printf("Error creating socket\n");
-}
+   int sockfd , clen , clientfd;
+   struct sockaddr_in saddr , caddr;
+   char* hello = "Hello ";
+   char buffer[1024] = { 0 };
+   unsigned short port = 8784;
+   if (( sockfd = socket(AF_INET , SOCK_STREAM , 0)) <0)
+   {
+       printf("Error creating socket\n");
+    }
 
-memset(&saddr , 0, sizeof(saddr));
-saddr.sin_family = AF_INET;
-saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-saddr.sin_port = htons(port);
+   memset(&saddr , 0, sizeof(saddr));
+   saddr.sin_family = AF_INET;
+   saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+   saddr.sin_port = htons(port);
 
-if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) <0)
-{
-    printf("Error binding\n");
-}
+   if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) <0)
+   {
+        printf("Error binding\n");
+   }
 
-if (listen(sockfd, 5 )<0)
-{
-    printf("Error listening\n");
-}
-else 
-{
+   if (listen(sockfd, 5 )<0)
+   {
+       printf("Error listening\n");
+   }
+   else 
+   {
     printf("Listen to port: %d\n", port);
-}
+   }
 
-clen = sizeof(caddr);
-if ((clientfd = accept(sockfd, (struct sockaddr * ) &caddr, &clen))  < 0)
-{
-    printf("Error accepting connection\n");
-}
+   clen = sizeof(caddr);
+   if ((clientfd = accept(sockfd, (struct sockaddr * ) &caddr, &clen))  < 0)
+   {
+       printf("Error accepting connection\n");
+   }
    send(sockfd, hello, strlen(hello), 0);
-    printf(" message sent\n");
-    clen = read(sockfd, buffer, 1024);
-    printf("%s\n", buffer);
- 
-    
+      printf(" message sent\n");
+   clen = read(sockfd, buffer, 1024);
+      printf("%s\n", buffer);
     close(clientfd);
     return 0;}
