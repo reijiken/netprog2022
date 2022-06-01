1e20d1c24558ce906056abef51969c71cf2dc8cb
reijiken <quyduongtran0810@gmail.com>
2022-06-01 08:59:57 +0700

    Update 04.practical.work.client.setup.c
    update lab4

--- 04.practical.work.client.setup.c
+++ 04.practical.work.client.setup.c
@@ -6,43 +6,44 @@
 #include <string.h>
 #include <stdlib.h>
  
-int main(int argc, char *argv[]){
-    struct sockaddr_in saddr;
-    struct hostent *h;
-    struct in_addr *address;
-    int sockfd;
-    unsigned short port = 8784;
-  
-    if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("Error creating socket\n");
-        return 1;
+int main(int argc, char *argv[])
+{
+  struct sockaddr_in saddr;
+  struct hostent *h;
+  struct in_addr *address;
+  int sockfd;
+  unsigned short port = 8784;
+
+  if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("Error creating socket\n");
+    return 1;
     }
-  
   char input[100];     
   if (argc > 1){
-        strcpy(input, argv[1]);
+    strcpy(input, argv[1]);
     }
-    else{
-        printf("Enter a name: ");
-        scanf("%s", input);
+  else{
+    printf("Enter a name: ");
+    scanf("%s", input);
     }
   printf("The domain is : %s\n",input);
   h=gethostbyname(input);
   address = (struct in_addr*) (h->h_addr);
   printf("%s has address %s\n",input,inet_ntoa(*address));
   if (h == NULL){
-        printf("Unknown host !!!\n");
-        return 0;
+    printf("Unknown host !!!\n");
+    return 0;
     }
-    
+
   memset(&saddr, 0, sizeof(saddr));
   saddr.sin_family = AF_INET;
   memcpy((char *) &saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
   saddr.sin_port = htons(port);
   
   if (connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
-     printf("Cannot connect \n");
-     return 1;}
+    printf("Cannot connect \n");
+    return 1;
+    }
   printf("Server connected.\n");
   return 0;
 }
